6337d403352ec49f1d2ddf1000465eb61d33e492
Dhara <dharacharolap@gmail.com>
2019-09-25 01:59:06 +0530

    Added some images and removed unwanted code

--- src/component/Products.js
+++ src/component/Products.js
@@ -39,43 +39,6 @@ export default class Products extends Component {
     return (
       <Col xs={6} sm={5} md={5} lg={5} className="product-display">
         {this.renderProducts()}
-        {/* <Card>
-          <Card.Img variant="top" src="assets/images/vegetablejuice.jpg" />
-          <Card.Body>
-            <Card.Title>Product Title</Card.Title>
-            <Card.Subtitle className="text-muted">Price</Card.Subtitle>
-          </Card.Body>
-        </Card>
-        <br />
-        <Card>
-          <Card.Img variant="top" src="assets/images/vegetablejuice.jpg" />
-          <Card.Body>
-            <Card.Title>Product Title</Card.Title>
-            <Card.Subtitle className="text-muted">Price</Card.Subtitle>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src="assets/images/vegetablejuice.jpg" />
-          <Card.Body>
-            <Card.Title>Product Title</Card.Title>
-            <Card.Subtitle className="text-muted">Price</Card.Subtitle>
-          </Card.Body>
-        </Card>
-        <br />
-        <Card>
-          <Card.Img variant="top" src="assets/images/vegetablejuice.jpg" />
-          <Card.Body>
-            <Card.Title>Product Title</Card.Title>
-            <Card.Subtitle className="text-muted">Price</Card.Subtitle>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src="assets/images/vegetablejuice.jpg" />
-          <Card.Body>
-            <Card.Title>Product Title</Card.Title>
-            <Card.Subtitle className="text-muted">Price</Card.Subtitle>
-          </Card.Body>
-        </Card> */}
       </Col>
     );
   }
